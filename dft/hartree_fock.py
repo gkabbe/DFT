0b80acf47d04c7fe185e7489ee6e91f10d8c5c87
gk <gabriel.kabbe@mail.de>
2017-07-12 00:43:17 +0200

    Add class Slater and super class OrbitalFunction

--- dft/hartree_fock.py
+++ dft/hartree_fock.py
@@ -3,9 +3,8 @@
 import numpy as np
 
 
-class Gaussian:
-    """Gaussian function"""
-    def __init__(self, alpha, center, prefactor=1):
+class OrbitalFunction:
+    def __init__(self, alpha, center, *, prefactor=1.0):
         self.alpha = alpha
         self.prefactor = prefactor
         if type(center) is np.array:
@@ -13,6 +12,16 @@ class Gaussian:
         else:
             self.center = np.array([center])
 
+
+class Slater(OrbitalFunction):
+    """Slater function"""
+    def __call__(self, x):
+        x = x.reshape((-1, self.center.shape[-1]))
+        return self.prefactor * (self.alpha**3 / np.pi)**0.5 * \
+               np.exp(-self.alpha * np.sqrt(np.sum((x - self.center)**2)))
+
+class Gaussian(OrbitalFunction):
+    """Gaussian function"""
     def __call__(self, x):
         x = x.reshape((-1, self.center.shape[-1]))
         return self.prefactor * (2 * self.alpha / np.pi)**0.75 * \
@@ -26,7 +35,7 @@ class Gaussian:
                         np.exp(-self.alpha * beta / (self.alpha + beta) *
                                np.sum((self.center - other.center)**2, axis=-1))
             new_center = (self.alpha * self.center + beta * other.center) / (self.alpha + beta)
-            return Gaussian(new_alpha, new_center, prefactor)
+            return Gaussian(new_alpha, new_center, prefactor=prefactor)
         else:
             return Gaussian(self.alpha, self.center, other)
 
