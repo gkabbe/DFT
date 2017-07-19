ad7cb581c5a2c360c94fbbd04ab84e2d3546c8f4
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2017-07-19 16:15:18 +0200

    Fix import in tests, add test_overlap

--- tests/test_orbitals.py
+++ tests/test_orbitals.py
@@ -1,5 +1,5 @@
 import numpy as np
-from dft import hartree_fock
+from dft.basis_sets import sto_ng
 
 from pytest import approx
 
@@ -7,13 +7,13 @@ from pytest import approx
 def test_evaluation():
     x = np.linspace(-3, 3)
 
-    gaussian = hartree_fock.Gaussian(1, 0)
+    gaussian = sto_ng.Gaussian(1, 0)
 
     y = gaussian(x)
 
 
 def test_gaussian_normalization():
-    gaussian = hartree_fock.Gaussian(1, 0)
+    gaussian = sto_ng.Gaussian(1, 0)
     x = np.linspace(-3, 3)
     y = gaussian(x)
     dx = x[1] - x[0]
@@ -21,7 +21,7 @@ def test_gaussian_normalization():
 
 def test_gaussian_multiplication_w_number():
     x = np.linspace(-3, 3)
-    g1 = hartree_fock.Gaussian(1, 0)
+    g1 = sto_ng.Gaussian(1, 0)
     g2 = g1 * 3
     g3 = 3 * g1
     g4 = 3 * g3
@@ -39,8 +39,8 @@ def test_gaussian_multiplication_w_number():
 
 def test_gaussian_multiplication_w_gaussian():
     x = np.linspace(-3, 3)
-    g1 = hartree_fock.Gaussian(2, -1)
-    g2 = hartree_fock.Gaussian(1, 1)
+    g1 = sto_ng.Gaussian(2, -1)
+    g2 = sto_ng.Gaussian(1, 1)
     g3 = g1 * g2
 
     y1 = g1(x)
@@ -54,7 +54,7 @@ def test_gaussian_multiplication_w_gaussian():
 
 def test_slater():
     x = np.linspace(-3, 3)
-    s1 = hartree_fock.Slater(1, 0)
+    s1 = sto_ng.Slater(1, 0)
     s2 = 3 * s1
     s3 = 3 * s2
     s4 = s2 * 3
@@ -67,19 +67,31 @@ def test_slater():
                                err_msg="Multiplication of Slater with number failed")
 
 
+def test_overlap():
+    gauss1 = sto_ng.Gaussian(1, 0)
+    gauss2 = sto_ng.Gaussian(1.5, 0)
+
+    analytical_result = sto_ng.overlap_integral(gauss1, gauss2)
+
+    x = np.linspace(-5, 5, 100)
+    y = (gauss1 * gauss2)(x, x, x)
+    numerical_result = np.sum(y) * (x[1] - x[0])**3
+
+    assert analytical_result == approx(numerical_result)
+
 # These three tests are not suited for unit testing (too long), but results are ok
 #
 # def test_sto_1g():
-#     sto_ng = hartree_fock.STO_NG([0, 0, 0], coefficients=[1], exponents=[1])
-#     coeff, expo = sto_ng.find_coeffs_and_exponents()
+#     sto = sto_ng.STO_NG([0, 0, 0], coefficients=[1], exponents=[1])
+#     coeff, expo = sto.find_coeffs_and_exponents()
 #
 #     # Assert equal result as Szabo/Ostlund (p. 157)
 #     assert expo[0] == approx(0.270950), "Result deviates from Szabo/Ostlund's result"
 #
 #
 # def test_sto_2g():
-#     sto_ng = hartree_fock.STO_NG([0, 0, 0], coefficients=[1, 1], exponents=[1, 1])
-#     coeff, expo = sto_ng.find_coeffs_and_exponents()
+#     sto = sto_ng.STO_NG([0, 0, 0], coefficients=[1, 1], exponents=[1, 1])
+#     coeff, expo = sto.find_coeffs_and_exponents()
 #
 #     # Assert equal result as Szabo/Ostlund (p. 157)
 #     assert all(coeff == approx([0.678914, 0.430129])), "Result deviates from Szabo/Ostlund's result"
@@ -87,8 +99,8 @@ def test_slater():
 #
 #
 # def test_sto_3g():
-#     sto_ng = hartree_fock.STO_NG([0, 0, 0], coefficients=[1, 1, 1], exponents=[1, 1, 1])
-#    coeff, expo = sto_ng.find_coeffs_and_exponents()
+#     sto = sto_ng.STO_NG([0, 0, 0], coefficients=[1, 1, 1], exponents=[1, 1, 1])
+#    coeff, expo = sto.find_coeffs_and_exponents()
 #
 #    # Assert equal result as Szabo/Ostlund (p. 157)
 #    assert all(coeff == approx([0.444635, 0.535328, 0.154329])), \
